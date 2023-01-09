b744f8dc3305aa52fde818e43f36700e7c352162
Jehan Veracierta <jehanzao@gmail.com>
2023-01-08 21:42:26 -0400

    feat: Conexion a DB, task model and migration

---
+++ app/Models/Task.ts
@@ -0,0 +1,31 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Task extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public tittle: string
+
+  @column()
+  public information: string
+
+  @column()
+  public task_date: DateTime
+
+  @column()
+  public complexity: string
+
+  @column()
+  public done: boolean
+
+  @column()
+  public archived: boolean
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
